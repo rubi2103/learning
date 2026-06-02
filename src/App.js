bc212e5fe3276ec2699fd06e37dbcb82f89c9087
rubi2103 <nagarubini2006@gmail.com>
2026-06-02 13:59:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setError("Please fill all fields");
+      return;
+    }
+
+    setError("");
+    alert("Login Successful");
+  };
+
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#f4f4f4"
+    }}>
+      <form
+        onSubmit={handleLogin}
+        style={{
+          background: "white",
+          padding: "30px",
+          borderRadius: "10px",
+          boxShadow: "0 4px 10px gray",
+          width: "300px"
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>
+          Student Learning Platform
+        </h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          style={{
+            width: "100%",
+            padding: "10px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 10px gray",
+            marginBottom: "10px"
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{
+            width: "100%",
+            padding: "10px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 10px gray",
+            marginBottom: "10px"
+          }}
+        />
+
+        {error && (
+          <p style={{ color: "red" }}>{error}</p>
+        )}
+
+        <button
+          type="submit"
+          style={{
+            width: "50%",
+            padding: "10px",
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            boxShadow: "0 4px 10px gray",
+            margin: "20px auto",
+            display: "block",
+            cursor: "pointer"
+          }}
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
